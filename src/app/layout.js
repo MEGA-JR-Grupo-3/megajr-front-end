b87b2584033b1f97240d0aac4442341428d506e5
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-16 09:40:29 -0400

    Adicionando imgs de icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Dosis } from "next/font/google";
 import "./globals.css";
 import RootLayoutClient from "./layout.client";
+import Head from "next/head"; // Importe o componente Head
 
 const dosis = Dosis({
   subsets: ["latin"],
@@ -13,5 +14,36 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <RootLayoutClient dosis={dosis}>{children}</RootLayoutClient>;
+  return (
+    <html lang="pt-BR">
+      <Head>
+        {/* Favicon padrão */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* Ícone para dispositivos iOS */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Ícones para Android Chrome */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
+      </Head>
+      <body className={dosis.className}>
+        <RootLayoutClient dosis={dosis}>{children}</RootLayoutClient>
+      </body>
+    </html>
+  );
 }
