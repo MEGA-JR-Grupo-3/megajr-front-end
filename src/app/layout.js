6020f864d2a4108b53ba19b379382d9972dda92d
enzovalencuela <esilvavalencuela@gmail.com>
2025-05-16 10:27:22 -0400

    Ajustando imgs icon no app e ajustando layout.js com os links

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,26 +7,6 @@ const dosis = Dosis({
   weight: ["400", "500", "600", "700"],
 });
 
-export const link = [
-  {
-    rel: "apple-touch-icon",
-    sizes: "180x180",
-    href: "/apple-touch-icon.png",
-  },
-  {
-    rel: "icon",
-    type: "image/png",
-    sizes: "512x512",
-    href: "/android-chrome-512x512.png",
-  },
-  {
-    rel: "icon",
-    type: "image/png",
-    sizes: "192x192",
-    href: "/android-chrome-192x192.png",
-  },
-];
-
 export const metadata = {
   title: "JubiTasks",
   description: "Gerencie suas tarefas com o seu amigo Jubileu!",
@@ -38,20 +18,28 @@ export const metadata = {
   openGraph: {
     title: "JubiTasks",
     description: "Gerencie suas tarefas com o seu amigo Jubileu!",
-    images: [
-      {
-        url: "./favicon.ico",
-        width: 500,
-        height: 500,
-        alt: "JubiTasks",
-      },
-    ],
+    images: ["/favicon.ico"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={dosis.className}>
         <RootLayoutClient dosis={dosis}>{children}</RootLayoutClient>
       </body>
